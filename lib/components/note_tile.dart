bf1ec0b4d1e4197e3cdc2ebed80f9ce803ca738a
gracebir <grace.devolop@gmail.com>
2024-02-18 15:16:44 +0200

    node tile component

---
+++ lib/components/note_tile.dart
@@ -0,0 +1,43 @@
+import 'package:fl_note_app/models/note.dart';
+import 'package:flutter/material.dart';
+
+class NoteTile extends StatelessWidget {
+  final String title;
+  final bool isSync;
+  NoteTile({super.key, required this.title, required this.isSync});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 23, vertical: 25),
+      margin: const EdgeInsets.symmetric(vertical: 8),
+      decoration: BoxDecoration(
+        color: const Color(0xffFDFFB6),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: Text(
+              title,
+              style: const TextStyle(fontSize: 18, color: Colors.black),
+            ),
+          ),
+          const SizedBox(
+            width: 8,
+          ),
+          isSync
+              ? Image.asset(
+                  "lib/assets/sync.png",
+                  height: 20,
+                )
+              : Image.asset(
+                  "lib/assets/no-sync.png",
+                  height: 20,
+                )
+        ],
+      ),
+    );
+  }
+}
