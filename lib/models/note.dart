4b68ad69a006e9989f973d0ee8a99ee0002c179c
gracebir <grace.devolop@gmail.com>
2024-02-18 15:17:07 +0200

    note model

---
+++ lib/models/note.dart
@@ -0,0 +1,12 @@
+class Note {
+  final int id;
+  final String title;
+  final String content;
+  final bool isSync;
+
+  Note(
+      {required this.id,
+      required this.title,
+      required this.content,
+      required this.isSync});
+}
