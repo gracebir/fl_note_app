310e6e47e09d27b0f7843e26cd94b7f6aea81174
gracebir <grace.devolop@gmail.com>
2024-02-18 15:17:50 +0200

    finalize the dashboard page

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -1,4 +1,5 @@
 import 'package:fl_note_app/components/app_nav.dart';
+import 'package:fl_note_app/components/note_tile.dart';
 import 'package:flutter/material.dart';
 
 class DashBoardPage extends StatelessWidget {
@@ -7,18 +8,45 @@ class DashBoardPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Column(
-        children: [
-          AppNav(),
-          const SizedBox(
-            height: 20,
-          ),
-          ListView(
-            children: [],
-          ),
-        ],
+      body: SafeArea(
+        child: Column(
+          children: [
+            const AppNav(),
+            Expanded(
+              child: ListView(
+                padding: const EdgeInsets.symmetric(horizontal: 25),
+                children: [
+                  NoteTile(title: "Digital Marketing", isSync: true),
+                  NoteTile(
+                      title:
+                          "Book Review: The Design of Everyday Things by Don Norman",
+                      isSync: false),
+                  NoteTile(
+                      title: "Good Design while Going Green", isSync: true),
+                  NoteTile(title: "Life Size Gaming Simulator", isSync: true),
+                  NoteTile(
+                      title: "The most dangerous is comfort", isSync: true),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
-    ));
+      floatingActionButton: GestureDetector(
+        onTap: () {},
+        child: Container(
+          width: 70,
+          height: 70,
+          padding: const EdgeInsets.all(10),
+          decoration: const BoxDecoration(
+              shape: BoxShape.circle, color: Color(0xffFF007A)),
+          child: Image.asset(
+            "lib/assets/plus.png",
+            height: 28,
+            width: 28,
+          ),
+        ),
+      ),
+    );
   }
 }
